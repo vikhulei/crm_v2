bd8bd3b53579999d2a77bef027ec4490ee4240ac
vikhulei <65869915+vikhulei@users.noreply.github.com>
2022-08-17 23:28:49 +0100

    test the link from codesandbox

--- src/pages/contactstore/profile/Profile.js
+++ src/pages/contactstore/profile/Profile.js
@@ -1,39 +1,51 @@
-import { DataBox } from "../../../components/ui/StyledComponents"
-import { DataBoxNavProfile, ImageWrapper, InputImage, Image, TopImageText, ErrorImageText, ErrorImageInfo, InfoWrapper, InfoLabel, InfoData, PasswordForm, Fieldset, Legend } from "./ProfileStyle"
-import camberbech from "../../../assets/camberbech.jpg"
+import { DataBox } from "../../../components/ui/StyledComponents";
+import {
+  DataBoxNavProfile,
+  ImageWrapper,
+  InputImage,
+  Image,
+  TopImageText,
+  ErrorImageText,
+  ErrorImageInfo,
+  InfoWrapper,
+  InfoLabel,
+  InfoData,
+  PasswordForm,
+  Fieldset,
+  Legend
+} from "./ProfileStyle";
+import camberbech from "../../../assets/camberbech.jpg";
 
 const Profile = () => {
-    return (
-        <>
-            <DataBox>
-                <DataBoxNavProfile>Profile</DataBoxNavProfile>
-                <TopImageText>Click the image to change it:</TopImageText>
-                <ImageWrapper>
-                    <Image src={camberbech} />
-                    <InputImage type="file" size="1" />
-                </ImageWrapper>
-                <ErrorImageText>The image was not uploaded</ErrorImageText>
-                <ErrorImageInfo>Errror accesing data</ErrorImageInfo>
-                <InfoWrapper>
-                    <InfoLabel>First Name:</InfoLabel>
-                    <InfoData>Doctor</InfoData>
-                    <InfoLabel>Last Name:</InfoLabel>
-                    <InfoData>Who</InfoData>
-                    <InfoLabel>Email:</InfoLabel>
-                    <InfoData>user3@intrinsicgrouplimited.com
-                    </InfoData>
-                    <InfoLabel>Join Date:</InfoLabel>
-                    <InfoData>2022-07-25T13:35:33.066+00:00
-                    </InfoData>
-                </InfoWrapper>
-                <PasswordForm>
-                    <Fieldset>
-                        <Legend>Change Password</Legend>
-                    </Fieldset>
-                </PasswordForm>
-            </DataBox>
-        </>
-    )
-}
+  return (
+    <>
+      <DataBox>
+        <DataBoxNavProfile>Profile</DataBoxNavProfile>
+        <TopImageText>Click the image to change it:</TopImageText>
+        <ImageWrapper>
+          <Image src={camberbech} />
+          <InputImage type="file" size="1" />
+        </ImageWrapper>
+        <ErrorImageText>The image was not uploaded</ErrorImageText>
+        <ErrorImageInfo>Errror accesing data</ErrorImageInfo>
+        <InfoWrapper>
+          <InfoLabel>First Name:</InfoLabel>
+          <InfoData>Doctorrr</InfoData>
+          <InfoLabel>Last Name:</InfoLabel>
+          <InfoData>Who</InfoData>
+          <InfoLabel>Email:</InfoLabel>
+          <InfoData>user3@intrinsicgrouplimited.com</InfoData>
+          <InfoLabel>Join Date:</InfoLabel>
+          <InfoData>2022-07-25T13:35:33.066+00:00</InfoData>
+        </InfoWrapper>
+        <PasswordForm>
+          <Fieldset>
+            <Legend>Change Password</Legend>
+          </Fieldset>
+        </PasswordForm>
+      </DataBox>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
