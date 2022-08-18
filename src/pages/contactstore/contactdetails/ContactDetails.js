f781198c459dc715ef1eac52f474ba17ffe1f60b
vikhulei <vikhulei@gmail.com>
2022-08-18 11:50:18 +0100

    Profile draft done

---
+++ src/pages/contactstore/contactdetails/ContactDetails.js
@@ -0,0 +1,13 @@
+import {DataBox, DataBoxNav} from "../../../components/ui/StyledComponents"
+
+const ContactList = () => {
+    return (
+        <>
+            <DataBox>
+                <DataBoxNav>Contact Details</DataBoxNav>
+            </DataBox>
+        </>
+    )
+}
+
+export default ContactList
